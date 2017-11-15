0c31ecea7257c12912d892788c60990dbadcdc17
Jason L Perry <jasper@ambethia.com>
2017-11-15 10:58:14 -0500

    Enable RPC and add Docker compose file. (#4)
    * Add compose file
    
    * Enable RPC
    
    * Break docker_run_and_create_wallet into stand alone create_wallet
    
    * Remove command from docker-compose.yml
    
    * Add description of changes to the README
    
    * Expose RPC ports in run script

---
+++ create_wallet.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+WALLET_PWD="coz"
+
+echo "Starting script to claim NEO and GAS..."
+CLAIM_CMD="python3.5 /opt/neo-python/contrib/privnet-claim-neo-and-gas.py -o /tmp/wallet -p ${WALLET_PWD} -w /tmp/wif"
+DOCKER_CMD="docker exec -it neo-privnet ${CLAIM_CMD}"
+echo $DOCKER_CMD
+echo
+($DOCKER_CMD)
+
+echo
+echo "Copying wallet file and wif key out of Docker container..."
+docker cp neo-privnet:/tmp/wif ./neo-privnet.wif
+docker cp neo-privnet:/tmp/wallet ./neo-privnet.wallet
+
+echo
+echo "--------------------"
+echo
+echo "All done! You now have 2 files in the current directory:"
+echo
+echo "  neo-privnet.wallet .. a wallet you can use with neo-python (pwd: ${WALLET_PWD})"
+echo "  neo-privnet.wif ..... a wif private key you can import into other clients"
+echo
+echo "Enjoy!"
