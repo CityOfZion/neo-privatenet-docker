2d8f7840ba4dddcea1b5ef1fb5bcdcc1cbcb6ddb
Ashley Rolleston <ash@block-tree.io>
2017-11-29 12:37:13 +0000

    fix missing syntax

--- docker_build.sh
+++ docker_build.sh
@@ -2,5 +2,6 @@
 if [ -z "$VAR" ]; 
     then wget -O /opt/neo-cli.zip https://github.com/neo-project/neo-cli/releases/download/v2.4.1/neo-cli-ubuntu.16.04-x64.zip
     else cp $1 /opt/neo-cli.zip
+fi
 
 docker build -t neo-privnet .
