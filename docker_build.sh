bb6b363825e247a9667ec185dc972d60d4568f80
Ashley Rolleston <ash@block-tree.io>
2017-11-29 12:43:57 +0000

    use correct var name

--- docker_build.sh
+++ docker_build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ -z "$VAR" ]; 
+if [ -z "$1" ]; 
     then wget -O /opt/neo-cli.zip https://github.com/neo-project/neo-cli/releases/download/v2.4.1/neo-cli-ubuntu.16.04-x64.zip
     else cp $1 /opt/neo-cli.zip
 fi
