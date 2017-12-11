12ae492dde827519221596b72efb26dfccf2aa63
Ashley Rolleston <ash@block-tree.io>
2017-12-11 13:48:03 +0000

    only download release if we haven't previously

--- docker_build.sh
+++ docker_build.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 if [ -z "$1" ]; 
-then
-    echo "no neo-cli.zip provided - fetching default neo-cli" 
-    wget -O ./neo-cli.zip https://github.com/neo-project/neo-cli/releases/download/v2.4.1/neo-cli-ubuntu.16.04-x64.zip
+then    
+    if [ -e neo-release241.zip ]
+    then
+        echo "no neo-cli.zip provided - release already downloaded" 
+    else
+        echo "no neo-cli.zip provided - release already downloaded" 
+        wget -O ./neo-release241.zip https://github.com/neo-project/neo-cli/releases/download/v2.4.1/neo-cli-ubuntu.16.04-x64.zip
+    fi
+    cp ./neo-release241.zip ./neo-cli.zip
 else
     echo "local neo-cli.zip provided - copying" 
     cp $1 ./neo-cli.zip
