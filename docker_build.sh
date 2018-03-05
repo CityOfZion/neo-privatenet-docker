2388b4da35d866fb03b39fc801900315a37fffb7
Chris Hager <chris@linuxuser.at>
2018-03-05 14:46:21 +0000

    docker_build.sh args: --no-cache and --neo-cli (#31)
    * docker_build.sh args: --no-cache and --neo-cli
    
    * Updated the README with the args for docker_build.sh

--- docker_build.sh
+++ docker_build.sh
@@ -4,15 +4,45 @@ set -e
 # To use a newer neo-cli version, just update this variable:
 NEO_CLI_VERSION="2.7.1"
 
+function usage {
+    echo "Usage: $0 [--no-cache] [--neo-cli <zip-fn>]"
+}
+
+while [[ "$#" > 0 ]]; do case $1 in
+    -h)
+        usage
+        exit 0
+        ;;
+    --no-cache)
+        DISABLE_CACHE=1
+        shift
+        ;;
+    --neo-cli)
+        # Custom neo-cli zip filename
+        NEO_CLI_CUSTOM_ZIPFN=$2
+        if [[ -z $NEO_CLI_CUSTOM_ZIPFN ]]; then
+            echo "Error: Please specify a neo-cli zip file"
+            usage
+            exit 1
+        fi
+        echo "Custom neo-cli zip: $NEO_CLI_CUSTOM_ZIPFN"
+        shift; shift
+        ;;
+    *)
+        usage
+        exit 1
+        ;;
+  esac;
+done
+
 # Definition of standard neo-cli filenames and URL based on the version
 NEO_CLI_ZIPFN="neo-release-${NEO_CLI_VERSION}.zip"
 NEO_CLI_URL="https://github.com/neo-project/neo-cli/releases/download/v${NEO_CLI_VERSION}/neo-cli-ubuntu.16.04-x64.zip"
 
-if [ -z "$1" ];
-then
-    echo "Using default neo-cli v${NEO_CLI_VERSION}"
+if [ -z "$NEO_CLI_CUSTOM_ZIPFN" ]; then
+    echo "Using downloaded neo-cli v${NEO_CLI_VERSION}"
 
-    if [ -e "${NEO_CLI_ZIPFN}" ]
+    if [ -e "${NEO_CLI_ZIPFN}" ] && [ -z "$DISABLE_CACHE" ]
     then
         echo "- release already downloaded: ${NEO_CLI_ZIPFN}"
     else
@@ -21,8 +51,13 @@ then
     fi
     cp $NEO_CLI_ZIPFN ./neo-cli.zip
 else
-    echo "Using custom neo-cli.zip: $1"
-    cp $1 ./neo-cli.zip
+    echo "Using custom neo-cli.zip: $NEO_CLI_CUSTOM_ZIPFN"
+    cp $NEO_CLI_CUSTOM_ZIPFN ./neo-cli.zip
 fi
 
-docker build -t neo-privnet .
+if [ -z "$DISABLE_CACHE" ]; then
+  docker build -t neo-privnet .
+else
+  echo "docker build no cache"
+  docker build --no-cache -t neo-privnet .
+fi
